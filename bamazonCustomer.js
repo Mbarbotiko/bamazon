cdb8239184696f1368d75b703440c952558e3828
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-06-03 20:03:10 -0500

    submit homework early, do more if have time, need a managers view and some validation fields for entry

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,8 +10,8 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    start();
+    displayInventory();
+    start();//figure out why this is running first, then running again after.
 });
 
 
@@ -21,20 +21,18 @@ function start() {
             {
                 name: "item",
                 type: "input",
-                message: "What is the ID of the product you would like to purchase?"
+                message: "What is the ID of the product you would like to purchase?\n"
             },
             {
                 name: "count",
                 type: "input",
-                message: "How many would you like to purchase?"
+                message: "How many would you like to purchase?\n"
             },
 
         ])//will need something to validate user input.
         .then(function (answer) {
             var item = answer.item;
             var quantity = answer.count;
-            console.log(item);
-            console.log(quantity);
             var queryDB = 'SELECT * FROM products WHERE?';
             connection.query(
                 queryDB, { id: item }, function (err, data) {
@@ -44,33 +42,53 @@ function start() {
                         console.log('No ID selected, try again');
                     } else {
                         var productArr = data[0];
-                        console.log(productArr);
+
 
                         if (quantity <= productArr.stock_quantity) {
                             console.log('Processing order.....')
 
-            
+
                             var updateDBQuery = 'UPDATE products SET stock_quantity = ' + (productArr.stock_quantity - quantity) + ' WHERE id = ' + item;
 
 
                             connection.query(updateDBQuery, function (err, data) {
                                 if (err) throw err;
-                                console.log(' Your order has been placed!')
+                                console.log(' Your order has been placed! Your total is $'+productArr.price*quantity);
+
+                                displayInventory();
                                 start();
                             })
 
 
                         } else {
                             console.log('Sorry there isnt enough in stock, please change quanitity and re-order');
+                            displayInventory();
                             start();
                         }
                     }
                 })
 
+
         })
 
-     
 }
+
+function displayInventory() {
+    queryStore = ' SELECT * FROM products';
+    connection.query(queryStore, function (err, data) {
+        if (err) throw err;
+        for (var i = 0; i < data.length; i++) {
+            var storeFront = `'Item ID: ` + data[i].id + '|| ' + 'Product: ' + ' ' + data[i].product_name +'|| '+'Category: ' + ' ' + data[i].department_name+'|| ' + 'Price: ' + '$' + data[i].price;
+            console.log(storeFront);
+
+        }
+        +'\n';
+
+    })
+}
+
+
+
 
 
 
